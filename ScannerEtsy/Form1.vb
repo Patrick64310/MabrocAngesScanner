edb7308b9dec30824f47195ed3c68efcf5df1779
Patrick64310 <patrick.schoulz@free.fr>
2026-04-27 21:06:56 +0200

    Update Form1.vb

--- ScannerEtsy/Form1.vb
+++ ScannerEtsy/Form1.vb
@@ -1,10 +1,11 @@
-
 Imports System.Windows.Forms
 
 Public Class Form1
     Inherits Form
 
     Public Sub New()
+        MessageBox.Show("Form1.New exécuté")   
+
         Me.Text = "Mabroc'Anges Scanner"
         Me.Width = 500
         Me.Height = 300
