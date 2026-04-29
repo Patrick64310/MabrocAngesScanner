e478a8c79c80c10b3e1b83fedfaf9fd9da23550a
Patrick64310 <patrick.schoulz@free.fr>
2026-04-29 22:11:07 +0200

    Update Program.vb

--- ScannerEtsy/Program.vb
+++ ScannerEtsy/Program.vb
@@ -1,13 +1,24 @@
 
 Imports System
 Imports System.Windows.Forms
+Imports System.Runtime.InteropServices
 
 Module Program
 
+    ' ✅ AppUserModelID : indispensable pour l’icône barre des tâches
+    <DllImport("shell32.dll", CharSet:=CharSet.Unicode)>
+    Private Sub SetCurrentProcessExplicitAppUserModelID(appID As String)
+    End Sub
+
     <STAThread>
     Sub Main()
+
+        ' ✅ ID unique pour Windows (choisis une valeur stable)
+        SetCurrentProcessExplicitAppUserModelID("MabrocAnges.ScannerEtsy")
+
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
+
         Application.Run(New Form1())
     End Sub
 
