21b31cc42be84dd14891ca635d963702eab19c24
Patrick64310 <patrick.schoulz@free.fr>
2026-05-01 19:46:33 +0200

    Update LedPanel.vb

--- ScannerEtsy/LedPanel.vb
+++ ScannerEtsy/LedPanel.vb
@@ -6,15 +6,23 @@ Imports System.Drawing.Drawing2D
 Public Class LedPanel
     Inherits Panel
 
+    Private pulseValue As Integer = 0
+    Private pulseDir As Integer = 1
+    Private WithEvents pulseTimer As New Timer With {.Interval = 40}
+
+    Public Property LedColor As Color = Color.Green
+
     Public Sub New()
         Me.Size = New Size(60, 60)
-        Me.BackColor = Color.Red
+        Me.BackColor = Color.Black
         Me.DoubleBuffered = True
+
         Me.SetStyle(ControlStyles.AllPaintingInWmPaint Or _
                     ControlStyles.OptimizedDoubleBuffer Or _
                     ControlStyles.UserPaint, True)
 
         UpdateRegion()
+        pulseTimer.Start()
     End Sub
 
     Protected Overrides Sub OnResize(e As EventArgs)
@@ -28,34 +36,67 @@ Public Class LedPanel
         Me.Region = New Region(path)
     End Sub
 
-    Protected Overrides Sub OnPaint(e As PaintEventArgs)
-        MyBase.OnPaint(e)
+    Private Sub PulseTick(sender As Object, e As EventArgs) Handles pulseTimer.Tick
+        pulseValue += pulseDir * 5
+        If pulseValue >= 60 Then pulseDir = -1
+        If pulseValue <= 0 Then pulseDir = 1
+        Me.Invalidate()
+    End Sub
 
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
         Dim g = e.Graphics
         g.SmoothingMode = SmoothingMode.AntiAlias
 
-        Dim r As New Rectangle(2, 2, Me.Width - 4, Me.Height - 4)
+        Dim center = New Point(Me.Width \ 2, Me.Height \ 2)
 
-        ' LED principale
-        Using brush As New SolidBrush(Me.BackColor)
-            g.FillEllipse(brush, r)
+        ' ===== HALO LUMINEUX EXTERNE =====
+        For i = 1 To 6
+            Dim alpha = Math.Max(0, 35 - i * 5 + pulseValue \ 4)
+            Using halo As New SolidBrush(Color.FromArgb(alpha, LedColor))
+                g.FillEllipse(
+                    halo,
+                    center.X - (30 + i),
+                    center.Y - (30 + i),
+                    (60 + i * 2),
+                    (60 + i * 2)
+                )
+            End Using
+        Next
+
+        ' ===== ANNEAU MÉTAL INDUSTRIEL =====
+        Dim metalRect As New Rectangle(2, 2, Me.Width - 4, Me.Height - 4)
+        Using metalBrush As New LinearGradientBrush(
+            metalRect,
+            Color.FromArgb(180, 180, 180),
+            Color.FromArgb(80, 80, 80),
+            LinearGradientMode.Vertical)
+            g.FillEllipse(metalBrush, metalRect)
         End Using
 
-        ' Reflet LED (effet brillant)
-        Using highlight As New SolidBrush(Color.FromArgb(70, 255, 255, 255))
+        ' ===== LED CENTRALE =====
+        Dim ledRect As New Rectangle(8, 8, Me.Width - 16, Me.Height - 16)
+        Using ledBrush As New LinearGradientBrush(
+            ledRect,
+            ControlPaint.Light(LedColor),
+            ControlPaint.Dark(LedColor),
+            LinearGradientMode.Vertical)
+            g.FillEllipse(ledBrush, ledRect)
+        End Using
+
+        ' ===== REFLET BRILLANT =====
+        Using highlight As New SolidBrush(Color.FromArgb(70, Color.White))
             g.FillEllipse(
                 highlight,
-                r.X + 6,
-                r.Y + 6,
-                r.Width \ 3,
-                r.Height \ 3
+                ledRect.X + 6,
+                ledRect.Y + 5,
+                ledRect.Width \ 2,
+                ledRect.Height \ 3
             )
         End Using
 
-        ' Bord sombre (anneau extérieur)
-        Using pen As New Pen(Color.FromArgb(120, Color.Black), 1)
-            g.DrawEllipse(pen, r)
+        ' ===== CONTOUR MÉTAL =====
+        Using pen As New Pen(Color.FromArgb(140, 0, 0, 0), 1)
+            g.DrawEllipse(pen, metalRect)
         End Using
     End Sub
-
 End Class
