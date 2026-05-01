48b53c59dae7ad3051dd2c20fdcb3e431e4a07e2
Patrick64310 <patrick.schoulz@free.fr>
2026-05-01 19:39:33 +0200

    Update LedPanel.vb

--- ScannerEtsy/LedPanel.vb
+++ ScannerEtsy/LedPanel.vb
@@ -7,10 +7,25 @@ Public Class LedPanel
     Inherits Panel
 
     Public Sub New()
-        Me.Width = 60
-        Me.Height = 60
+        Me.Size = New Size(60, 60)
         Me.BackColor = Color.Red
         Me.DoubleBuffered = True
+        Me.SetStyle(ControlStyles.AllPaintingInWmPaint Or _
+                    ControlStyles.OptimizedDoubleBuffer Or _
+                    ControlStyles.UserPaint, True)
+
+        UpdateRegion()
+    End Sub
+
+    Protected Overrides Sub OnResize(e As EventArgs)
+        MyBase.OnResize(e)
+        UpdateRegion()
+    End Sub
+
+    Private Sub UpdateRegion()
+        Dim path As New GraphicsPath()
+        path.AddEllipse(0, 0, Me.Width, Me.Height)
+        Me.Region = New Region(path)
     End Sub
 
     Protected Overrides Sub OnPaint(e As PaintEventArgs)
@@ -21,18 +36,13 @@ Public Class LedPanel
 
         Dim r As New Rectangle(2, 2, Me.Width - 4, Me.Height - 4)
 
-        ' Ombre externe
-        Using shadow As New SolidBrush(Color.FromArgb(40, Color.Black))
-            g.FillEllipse(shadow, r.X - 2, r.Y - 2, r.Width + 4, r.Height + 4)
-        End Using
-
         ' LED principale
         Using brush As New SolidBrush(Me.BackColor)
             g.FillEllipse(brush, r)
         End Using
 
-        ' Reflet
-        Using highlight As New SolidBrush(Color.FromArgb(60, Color.White))
+        ' Reflet LED (effet brillant)
+        Using highlight As New SolidBrush(Color.FromArgb(70, 255, 255, 255))
             g.FillEllipse(
                 highlight,
                 r.X + 6,
@@ -41,6 +51,11 @@ Public Class LedPanel
                 r.Height \ 3
             )
         End Using
-    End Sub
-End Class
 
+        ' Bord sombre (anneau extérieur)
+        Using pen As New Pen(Color.FromArgb(120, Color.Black), 1)
+            g.DrawEllipse(pen, r)
+        End Using
+    End Sub
+
+End Class
