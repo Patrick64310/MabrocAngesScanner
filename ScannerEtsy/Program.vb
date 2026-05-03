8b164d10d105fffe6f705193c459f852c110b15f
Patrick64310 <patrick.schoulz@free.fr>
2026-05-03 17:14:11 +0200

    Update Program.vb

--- ScannerEtsy/Program.vb
+++ ScannerEtsy/Program.vb
@@ -12,18 +12,11 @@ Module Program
  
     <STAThread>
     Sub Main()
-
+        SetCurrentProcessExplicitAppUserModelID("MabrocAnges.ScannerEtsy")
+        ApplicationConfiguration.Initialize()
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
-
-        Application.Run(New Form1()
-
-        ' ✅ ID unique pour Windows (choisis une valeur stable)
-        'SetCurrentProcessExplicitAppUserModelID("MabrocAnges.ScannerEtsy")
-        'ApplicationConfiguration.Initialize()
-        'Application.EnableVisualStyles()
-        'Application.SetCompatibleTextRenderingDefault(False)
-        'Application.Run(New Form1())
+        Application.Run(New Form1())
     End Sub
 
 End Module
