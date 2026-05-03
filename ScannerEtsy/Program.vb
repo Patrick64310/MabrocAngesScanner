7d6cebafe95fe79aabfa7387364241161622549b
Patrick64310 <patrick.schoulz@free.fr>
2026-05-03 17:19:59 +0200

    Update Program.vb

--- ScannerEtsy/Program.vb
+++ ScannerEtsy/Program.vb
@@ -5,18 +5,18 @@ Imports System.Runtime.InteropServices
 
 Module Program
 
-    ' ✅ AppUserModelID : indispensable pour l’icône barre des tâches
+    ' ✅ AppUserModelID : icône correcte dans la barre des tâches / tray
     <DllImport("shell32.dll", CharSet:=CharSet.Unicode)>
     Private Sub SetCurrentProcessExplicitAppUserModelID(appID As String)
     End Sub
- 
+
     <STAThread>
     Sub Main()
+        ' Identifiant Windows stable
         SetCurrentProcessExplicitAppUserModelID("MabrocAnges.ScannerEtsy")
-        ApplicationConfiguration.Initialize()
+        ' ✅ Initialisation WinForms CORRECTE pour VB.NET
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New Form1())
-    End Sub
 
 End Module
